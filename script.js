ac63fe509fea32f5ecf8bcb8f9e1aad859a91b60
Coxik <coxik.coxik@gmail.com>
2019-10-06 11:00:34 +0300

    Final*

---
+++ script.js
@@ -0,0 +1,20 @@
+let arr = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+arr.push("-1");
+alert(arr);
+
+arr.unshift("1000");
+alert(arr);
+
+arr.push(["Last array element"]);
+alert(arr);
+
+let results = arr.filter(function(Javascript) {
+  });
+  alert(arr);
+
+let positiveArr = arr.filter(function(number) {
+    return number > 0;
+  });
+  alert(arr);
+
+  
